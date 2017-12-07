21ecaf5c16989aadb187629d1ec1fc226d0db20f
alex <geniofuturo@gmail.com>
2017-12-07 20:49:56 -0200

    Running the command and returning the output.

--- commands_async/tasks.py
+++ commands_async/tasks.py
@@ -1,8 +1,12 @@
+import cStringIO
+
 from celery import shared_task
 from django.core.management import call_command
 
 
 @shared_task(ignore_result=False)
-def cmdexec(name):
+def command_exec(name, *args, **kwargs):
     """ Run a Django command by name """
-    call_command(name)
+    kwargs['stdout'] = cStringIO.StringIO()
+    call_command(name, *args, **kwargs)
+    return kwargs['stdout'].getvalue()
