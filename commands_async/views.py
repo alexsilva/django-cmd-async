f93992928d3dc1752677a2070fe9efaf62ba9d76
alex <geniofuturo@gmail.com>
2017-12-07 18:49:56 -0200

    HTML structure with bootstrap and some functionality.

--- commands_async/views.py
+++ commands_async/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import FormView
+
 from .forms import TaskForm
+from django.core.management import get_commands
 
 
 class TaskFormView(FormView):
@@ -9,3 +11,17 @@ class TaskFormView(FormView):
 
     form_class = TaskForm
     success_url = '.'
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskFormView, self).get_context_data(**kwargs)
+
+        commands = {}
+        commands_all = get_commands()
+
+        # merge
+        for name in commands_all:
+            items = commands.setdefault(commands_all[name], [])
+            items.append(name)
+
+        context['commands'] = commands
+        return context
