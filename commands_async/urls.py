37eedc5a302aeaa13884ca3936c046930ef50693
alex <geniofuturo@gmail.com>
2017-12-07 17:17:13 -0200

    Registering urls.

--- commands_async/urls.py
+++ commands_async/urls.py
@@ -1,7 +1,14 @@
 # coding=utf-8
 from django.conf.urls import url, include
+from . import views
 
 __author__ = 'alex'
 
+
+cmd_patterns = ([
+    url(r'^$', views.TaskFormView.as_view(), name='index'),
+], 'cmd-async')
+
 urlpatterns = [
+    url(r'^cmd-async/', include(cmd_patterns)),
 ]
