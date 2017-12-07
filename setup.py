fe8cb95b7d25ae2a5f431d2b1feed30186552a1c
alex <geniofuturo@gmail.com>
2017-12-07 16:51:08 -0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='django-cmd-async',
+    version='1.0.0',
+    packages=['django_cmd_async'],
+    url='https://github.com/alexsilva/django-cmd-async',
+    license='MIT',
+    author='alex',
+    author_email='alex@fabricadigital.com.br',
+    description='Web execution of commands asynchronously.'
+)
