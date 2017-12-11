d0fe935af0c6ea97c69c50dbcb09e4e6791319d3
alex <geniofuturo@gmail.com>
2017-12-11 21:57:07 -0200

    Capturing stdout.

--- commands_async/tasks.py
+++ commands_async/tasks.py
@@ -1,13 +1,21 @@
-import cStringIO
+import StringIO
 
 from celery import shared_task
 from django.core.management import call_command
+import sys
 
 
 @shared_task(ignore_result=False,
              track_started=True)
 def command_exec(name, *args, **kwargs):
     """ Run a Django command by name """
-    kwargs['stdout'] = cStringIO.StringIO()
-    call_command(name, *args, **kwargs)
+    stream = StringIO.StringIO()
+    stream.encoding = sys.stdin.encoding  # hack
+    kwargs['stdout'] = stream
+    old_stdout = sys.stdout
+    sys.stdout = stream
+    try:
+        call_command(name, *args, **kwargs)
+    finally:
+        sys.stdout = old_stdout
     return kwargs['stdout'].getvalue()
