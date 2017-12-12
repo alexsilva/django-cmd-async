050ddd58633ea2ce25c52f6ee432667e11524713
alex <geniofuturo@gmail.com>
2017-12-11 22:30:58 -0200

    Capturing error with code 0

--- commands_async/tasks.py
+++ commands_async/tasks.py
@@ -16,6 +16,9 @@ def command_exec(name, *args, **kwargs):
     sys.stdout = stream
     try:
         call_command(name, *args, **kwargs)
+    except SystemExit as err:
+        if hasattr(err, 'code') and err.code != 0:
+            raise
     finally:
         sys.stdout = old_stdout
     return kwargs['stdout'].getvalue()
