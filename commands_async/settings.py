e499e21c2641b943f0de7262a03ad7ca3e878232
alex <geniofuturo@gmail.com>
2017-12-12 19:23:46 -0200

    Fixing logout bug and executing a command that invalidates the session.

--- commands_async/settings.py
+++ commands_async/settings.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
 
 # Whether to display commands in list format
@@ -7,4 +8,6 @@ COMMANDS_ASYNC_LIST = getattr(settings, "COMMANDS_ASYNC_LIST", True)
 COMMANDS_ASYNC_LOGIN_URL = getattr(settings, "COMMANDS_ASYNC_LOGIN_URL", settings.LOGIN_URL)
 COMMANDS_ASYNC_COMMANDS_IGNORE = getattr(settings, "COMMANDS_ASYNC_COMMANDS_IGNORE", [])
 COMMANDS_ASYNC_PERMISSION_NAME = getattr(settings, "COMMANDS_ASYNC_PERMISSION_NAME", None)
+COMMANDS_ASYNC_REDIRECT_FIELD_NAME = REDIRECT_FIELD_NAME
+
 
