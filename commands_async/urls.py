80f7a5c4f62c948ee95120ff0e2596e36f218ce4
alex <geniofuturo@gmail.com>
2017-12-12 11:01:39 -0200

    setup login required decorator.

--- commands_async/urls.py
+++ commands_async/urls.py
@@ -1,14 +1,17 @@
 # coding=utf-8
 from django.conf.urls import url, include
 from django.views.decorators.cache import never_cache
+from django.contrib.auth.decorators import login_required
 from . import views
 
 __author__ = 'alex'
 
 
 cmd_patterns = ([
-    url(r'^$', never_cache(views.TaskFormView.as_view()), name='index'),
-    url(r'^status/(?P<task_id>.*)', never_cache(views.TaskFormStatus.as_view()), name='status')
+    url(r'^$', login_required(never_cache(views.TaskFormView.as_view())),
+        name='index'),
+    url(r'^status/(?P<task_id>.*)', login_required(never_cache(views.TaskFormStatus.as_view())),
+        name='status')
 ], 'cmd-async')
 
 urlpatterns = [
