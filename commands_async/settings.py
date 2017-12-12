768db7851d4b86d6aeda17b33df75a5b77f13a92
alex <geniofuturo@gmail.com>
2017-12-12 14:28:19 -0200

    New configuration variables.

--- commands_async/settings.py
+++ commands_async/settings.py
@@ -1,5 +1,8 @@
+# coding=utf-8
 from django.conf import settings
 
 
 # Whether to display commands in list format
 COMMANDS_ASYNC_LIST = getattr(settings, "COMMANDS_ASYNC_LIST", True)
+COMMANDS_ASYNC_LOGIN_URL = getattr(settings, "COMMANDS_ASYNC_LOGIN_URL", settings.LOGIN_URL)
+COMMANDS_ASYNC_COMMANDS_IGNORE = getattr(settings, "COMMANDS_ASYNC_COMMANDS_IGNORE", [])
