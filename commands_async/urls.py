294a6d6e9cb891163270ab5f48a89675116cdf86
alex <geniofuturo@gmail.com>
2017-12-12 16:57:21 -0200

    Configuring the login url.

--- commands_async/urls.py
+++ commands_async/urls.py
@@ -2,15 +2,17 @@
 from django.conf.urls import url, include
 from django.views.decorators.cache import never_cache
 from django.contrib.auth.decorators import login_required
-from . import views
+from . import views, settings
 
 __author__ = 'alex'
 
 
 cmd_patterns = ([
-    url(r'^$', login_required(never_cache(views.TaskFormView.as_view())),
+    url(r'^$', login_required(never_cache(views.TaskFormView.as_view()),
+                              login_url=settings.COMMANDS_ASYNC_LOGIN_URL),
         name='index'),
-    url(r'^status/(?P<task_id>.*)', login_required(never_cache(views.TaskFormStatus.as_view())),
+    url(r'^status/(?P<task_id>.*)', login_required(never_cache(views.TaskFormStatus.as_view()),
+                                                   login_url=settings.COMMANDS_ASYNC_LOGIN_URL),
         name='status')
 ], 'cmd-async')
 
