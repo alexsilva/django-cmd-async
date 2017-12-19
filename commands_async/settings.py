860e2274b6e598543f9cb700217d8858f8f0e0e1
alex <geniofuturo@gmail.com>
2017-12-19 11:52:03 -0200

    Changing COMMANDS_ASYNC_LIST to False.

--- commands_async/settings.py
+++ commands_async/settings.py
@@ -4,7 +4,7 @@ from django.contrib.auth import REDIRECT_FIELD_NAME
 
 
 # Whether to display commands in list format
-COMMANDS_ASYNC_LIST = getattr(settings, "COMMANDS_ASYNC_LIST", True)
+COMMANDS_ASYNC_LIST = getattr(settings, "COMMANDS_ASYNC_LIST", False)
 COMMANDS_ASYNC_LOGIN_URL = getattr(settings, "COMMANDS_ASYNC_LOGIN_URL", settings.LOGIN_URL)
 COMMANDS_ASYNC_COMMANDS_IGNORE = getattr(settings, "COMMANDS_ASYNC_COMMANDS_IGNORE", [])
 COMMANDS_ASYNC_PERMISSION_NAME = getattr(settings, "COMMANDS_ASYNC_PERMISSION_NAME", None)
