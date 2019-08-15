0a8e61b7ab7c1696bae10990091ac122f729063c
alexsilva <geniofuturo@gmail.com>
2019-08-15 17:15:46 -0300

    Better stdout redirect.

--- commands_async/tasks.py
+++ commands_async/tasks.py
@@ -5,20 +5,39 @@ from django.core.management import call_command
 import sys
 
 
+class Output(object):
+    encoding = 'utf-8'
+
+    def __init__(self, stream):
+        self.stream = stream
+        self.stdout = sys.stdout
+
+    def __enter__(self):
+        sys.stdout = self.stream
+        return self
+
+    def __getattr__(self, name):
+        return getattr(object.__getattribute__(self, 'stream'), name)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        sys.stdout = self.stdout
+
+    def __str__(self):
+        return self.stream.getvalue()
+
+
 @shared_task(ignore_result=False,
              track_started=True)
 def command_exec(name, *args, **kwargs):
     """ Run a Django command by name """
-    stream = StringIO.StringIO()
-    stream.encoding = sys.stdin.encoding  # hack
-    kwargs['stdout'] = stream
-    old_stdout = sys.stdout
-    sys.stdout = stream
+    stream = Output(StringIO.StringIO())
     try:
-        call_command(name, *args, **kwargs)
-    except SystemExit as err:
-        if hasattr(err, 'code') and err.code != 0:
-            raise
+        with stream:
+            kwargs['stdout'] = stream
+            try:
+                call_command(name, *args, **kwargs)
+            except SystemExit as err:
+                if hasattr(err, 'code') and err.code != 0:
+                    raise
     finally:
-        sys.stdout = old_stdout
-    return kwargs['stdout'].getvalue()
+        return str(stream)
