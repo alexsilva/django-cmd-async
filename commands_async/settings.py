fabae7e6e70a3b5f5eb689313a4dfc54a1f0df62
alexsilva <geniofuturo@gmail.com>
2019-08-27 16:04:18 -0300

    task options settings.

--- commands_async/settings.py
+++ commands_async/settings.py
@@ -2,6 +2,12 @@
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
 
+COMMANDS_ASYNC_TASK_OPTIONS = dict(
+    expires=60 * 30,  # 30 min
+    retries=0
+)
+COMMANDS_ASYNC_TASK_OPTIONS.update(getattr(settings, 'COMMANDS_ASYNC_TASK_OPTIONS', {}))
+COMMANDS_ASYNC_TASK_PRIORITY = COMMANDS_ASYNC_TASK_OPTIONS.pop('priority', 0)
 
 # Whether to display commands in list format
 COMMANDS_ASYNC_LIST = getattr(settings, "COMMANDS_ASYNC_LIST", False)
@@ -10,3 +16,4 @@ COMMANDS_ASYNC_COMMANDS_IGNORE = getattr(settings, "COMMANDS_ASYNC_COMMANDS_IGNO
 COMMANDS_ASYNC_PERMISSION_NAME = getattr(settings, "COMMANDS_ASYNC_PERMISSION_NAME", None)
 COMMANDS_ASYNC_REDIRECT_FIELD_NAME = REDIRECT_FIELD_NAME
 COMMANDS_ASYNC_WORKERS_STATUS_CHECK = getattr(settings, 'COMMANDS_ASYNC_WORKERS_STATUS_CHECK', False)
+
